7e9e46077669d56eb9dd55a4b0b62fe76173216f
leonardoandresd <leonardoandresd@gmail.com>
2022-12-06 22:32:42 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+num = int(input("Por Favor ingrese un número para comprobar si es par o impar: "))
+if num % 2 == 0:
+    print("El número ingresado",num, "es par")
+else:
+    print("El número ingresado",num, "es impar")
+
